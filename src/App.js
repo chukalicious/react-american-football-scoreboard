5fc3948c61cca7e325a2ee1e950e94c9ec12533f
Katiuska <katiuskaalicea@gmail.com>
2020-07-22 00:10:59 -0700

    added event listener to 2 buttons on step 4

--- src/App.js
+++ src/App.js
@@ -33,11 +33,11 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 1)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 1)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
