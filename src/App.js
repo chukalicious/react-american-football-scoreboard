181def8ab21446e5245b1a61f09cb03fa3484662
Katiuska <katiuskaalicea@gmail.com>
2020-07-22 00:00:29 -0700

    completed step 3

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import "./App.css";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
-  const [homeScore, setScore] = useState(0); 
-  // const [awayScore, setScore] = useState(0); 
+  const [homeScore, setHomeScore] = useState(0); 
+  const [awayScore, setAwayScore] = useState(0); 
+  const[timer, setTimer] = useState("00:00");
+  const [awayName, setAwayName] =useState('Tigers')
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -18,10 +21,10 @@ function App() {
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{timer}</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <h2 className="away__name">{awayName}</h2>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
